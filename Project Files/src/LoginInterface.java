a27dfa455b683382c5a6c62a04961bd14fa57b3a
JustinDodemaide <103222511+JustinDodemaide@users.noreply.github.com>
2024-04-23 14:13:52 -0700

    Layout Changes

--- Project Files/src/LoginInterface.java
+++ Project Files/src/LoginInterface.java
@@ -3,13 +3,19 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class LoginInterface extends JPanel implements State {
+public class LoginInterface implements State {
+    private JFrame frame = new JFrame();
+	
     private JTextField usernameField;
     private JPasswordField passwordField;
     private JButton loginButton;
-
+    
     public LoginInterface() {
-        setLayout(new GridLayout(6, 1)); // interface is set up in a single column
+		frame.setTitle("Login");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	
+    	JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(6, 1)); // interface is set up in a single column
 
         JLabel usernameLabel = new JLabel("Username");
         usernameField = new JTextField(); // a text field for users to input their username
@@ -20,24 +26,25 @@ public class LoginInterface extends JPanel implements State {
         createAccountPanel.add(createAccountLabel);
         JButton loginButton = new JButton("Login");
 
-        add(usernameLabel);
-        add(usernameField);
-        add(passwordLabel);
-        add(passwordField);
-        add(createAccountPanel);
-        add(loginButton);
+        panel.add(usernameLabel);
+        panel.add(usernameField);
+        panel.add(passwordLabel);
+        panel.add(passwordField);
+        panel.add(createAccountPanel);
+        panel.add(loginButton);
         // adding components to panel
-       
+        
+        frame.add(panel);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        
         loginButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
             	String username = usernameField.getText(); // validates input
                 char[] passwordChars = passwordField.getPassword();
                 String password = new String(passwordChars);
                 
-                System.out.println("Username: " + username);
-                System.out.println("Password: " + password);
-
-                enter(); // once password is entered and is verified
+                // once password is entered and is verified
                 // move to the main control interface
 
             }
@@ -45,12 +52,13 @@ public class LoginInterface extends JPanel implements State {
     }
 
     public void enter() {
-        setVisible(false); // hides the log in interface once they hit enter
+    	frame.setVisible(true);
 
-        StateMachine.showMainControls(); // moves on to the main control once login is successful
+        // StateMachine.showMainControls(); // moves on to the main control once login is successful
     }
 
     public void exit() {
+        // setVisible(false); // hides the log in interface once they hit enter
 
     }
 }
