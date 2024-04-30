bf7ba548cbbced746b1b47ed76e1b6d7b831f5a9
nicoSavaides <savaidesnico@gmail.com>
2024-04-29 19:19:51 -0700

    JUnit tests for the Account class

--- Project Files/src/Account.java
+++ Project Files/src/Account.java
@@ -1,49 +1,47 @@
-package bankingPackage;
 
 public class Account {
 	private String id;
 	private String type;
 	private double total;
-	
+
 	public Account(String id, String type, double total) {
 		this.id = id;
 		this.type = type;
 		this.total = total;
 	}
-	
+
 	public String getID() {
 		return id;
 	}
-	
+
 	public String getType() {
 		return type;
 	}
-	
+
 	public double getTotal() {
 		return total;
 	}
-	
+
 	public void addToTotal(double amount) {
 		total = total + amount;
 		update();
 	}
-	
+
 	public void removeFromTotal(double amount) {
 
 		if ((total - amount) < 0) {
 			System.out.println("Cannot withdraw as overdraft fees would incur...");
 			return;
-		}
-		else {
+		} else {
 			total = total - amount;
 			update();
 		}
 	}
-	
+
 	public void update() {
 		StateMachine.client.updateAccount(this);
 	}
-	
+
 	public String toString() {
 		String newString = this.id + "/" + this.type + "/" + this.total;
 		return newString;
--- Project Files/src/Client.java
+++ Project Files/src/Client.java
@@ -1,24 +1,20 @@
 import java.io.BufferedReader;
-import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Client {
 	private Socket socket;
 	private PrintWriter out;
 	private BufferedReader in;
-	
-	public Client(){
-	
+
+	public Client() {
+
 	}
-	
+
 	public boolean connectToServer() {
 		// Connect to server
 		try {
@@ -32,7 +28,7 @@ public class Client {
 			e.printStackTrace();
 			return false;
 		}
-		
+
 		// Initialize input/output streams
 		try {
 			out = new PrintWriter(socket.getOutputStream(), true);
@@ -40,22 +36,22 @@ public class Client {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        try {
+		try {
 			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        
+
 		return true;
 	}
-	
+
 	public void disconect() {
-		if(socket == null) {
+		if (socket == null) {
 			System.out.println("Attempting to disconnect while not connected to server");
 			return;
 		}
-		
+
 		try {
 			socket.close();
 		} catch (IOException e) {
@@ -63,7 +59,7 @@ public class Client {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private String[] sendMessage(String message) {
 		System.out.println("Client sending message: " + message + ". Awaiting response...");
 		out.println(message);
@@ -75,44 +71,44 @@ public class Client {
 			System.err.println("Unable to read server reply");
 			e.printStackTrace();
 		}
-		
+
 		System.out.println("Client received response: " + response);
 		// Parse response
 		String[] tokens = parseMessage(response);
 		return tokens;
 	}
-	
+
 	public boolean attemptLogin(String username, String password) {
-		String loginMessage = createMessage(new String[]{"authenticateUser",username,password});
+		String loginMessage = createMessage(new String[] { "authenticateUser", username, password });
 		String[] response = sendMessage(loginMessage);
-		
+
 		final int STATUS = 0;
-		if(response[STATUS].equals("user not found"))
+		if (response[STATUS].equals("user not found"))
 			return false;
-		
+
 		ArrayList<String> ids = new ArrayList<String>();
-		for(int i = 2; i < response.length; i++)
+		for (int i = 2; i < response.length; i++)
 			ids.add(response[i]);
-		
+
 		// If user exists and pw was correct, initialize user
-		StateMachine.user = new User(username,password,ids);
+		StateMachine.user = new User(username, password, ids);
 		return true;
 	}
-	
+
 	public boolean attemptNewUser(String username, String password) {
-		String newUserMessage = createMessage(new String[]{"adduser",username,password});
+		String newUserMessage = createMessage(new String[] { "adduser", username, password });
 		String[] response = sendMessage(newUserMessage);
-		
+
 		final int STATUS = 0;
-		if(response[STATUS].equals("failed"))
+		if (response[STATUS].equals("failed"))
 			return false;
-		
+
 		// If new user was created, initialize user
-		StateMachine.user = new User(username,password,new ArrayList<String>());
+		StateMachine.user = new User(username, password, new ArrayList<String>());
 		System.out.println("Successfully created new user");
 		return true;
 	}
-	
+
 	public void updateUser(User user) {
 		int numAccounts = user.getAccounts().size();
 		System.out.println("numAccounts: " + Integer.toString(numAccounts));
@@ -121,7 +117,7 @@ public class Client {
 		info[1] = user.getUsername();
 		info[2] = user.getPIN();
 		ArrayList<Account> accounts = user.getAccounts();
-		for(int i = 0; i < numAccounts; i++) {
+		for (int i = 0; i < numAccounts; i++) {
 			info[i + 3] = accounts.get(i).getID();
 			System.out.println("account id " + Integer.toString(i) + ": " + info[i + 3]);
 		}
@@ -129,48 +125,50 @@ public class Client {
 		String updateUserMessage = createMessage(info);
 		sendMessage(updateUserMessage);
 	}
-	
+
 	public Account getAccount(String id) {
 		// command: getaccount
 		// Expect response: id, type, amount
-		String getAccountMessage = createMessage(new String[]{"getaccount",id});
+		String getAccountMessage = createMessage(new String[] { "getaccount", id });
 		String[] response = sendMessage(getAccountMessage);
-		
-		Account account = new Account(response[0],response[1],Double.parseDouble(response[2]));
+
+		Account account = new Account(response[0], response[1], Double.parseDouble(response[2]));
 		return account;
 	}
-	
+
 	public Account newAccount(String type) {
 		// command: newaccount
-		String newAccountMessage = createMessage(new String[]{"newaccount",type});
+		String newAccountMessage = createMessage(new String[] { "newaccount", type });
 		String[] response = sendMessage(newAccountMessage);
 		// expected response: id, type, "0.0";
-				
-		Account newAccount = new Account(response[0],response[1],Double.parseDouble(response[2]));
+
+		Account newAccount = new Account(response[0], response[1], Double.parseDouble(response[2]));
 		return newAccount;
 	}
-	
+
 	public void updateAccount(Account account) {
-		// Called by accounts when they're changed by MainControl action (deposit, transfer, ...)
+		// Called by accounts when they're changed by MainControl action (deposit,
+		// transfer, ...)
 		// command: updateaccount
-		String updateAccountMessage = createMessage(new String[]{"updateaccount",account.getID(),account.getType(),Double.toString(account.getTotal())});
+		String updateAccountMessage = createMessage(new String[] { "updateaccount", account.getID(), account.getType(),
+				Double.toString(account.getTotal()) });
 		sendMessage(updateAccountMessage); // Response isn't needed
 	}
-	
+
 	public void removeAccount(Account account) {
-		String removeAccountMessage = createMessage(new String[]{"removeaccount",account.getID()});
+		String removeAccountMessage = createMessage(new String[] { "removeaccount", account.getID() });
 		sendMessage(removeAccountMessage); // Response isn't needed
 
 	}
-	
+
 	private String createMessage(String[] tokens) {
 		String message = "";
-		for(String s : tokens) {
+		for (String s : tokens) {
 			message += s + ",";
 		}
 		return message;
 	}
-	
+
 	private String[] parseMessage(String message) {
 		return message.split(",");
 	}
